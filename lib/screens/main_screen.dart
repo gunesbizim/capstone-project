a3d30594a13d644837c29ba7a139b1873895f4d5
hazarcandoga.bakan <hazarcandogabakan@gmail.com>
2021-05-20 20:03:32 +0300

    Color File

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:capstone_project/constant_data/app_colors.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
