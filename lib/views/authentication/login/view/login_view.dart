d48e2f6252557e3989ccdb401487c663b3576b4b
hazarcandoga.bakan <hazarcandogabakan@gmail.com>
2021-05-22 23:11:08 +0300

    homepage

--- lib/views/authentication/login/view/login_view.dart
+++ lib/views/authentication/login/view/login_view.dart
@@ -11,7 +11,7 @@ import 'package:flutter/services.dart';
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //TODO: connect it with an ypper widget
+    //TODO: connect it with an upper widget
     MediaQueryData data = MediaQuery.of(context);
     double height = data.size.height;
     return BaseView<LoginViewModel>(
