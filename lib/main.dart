1d64361d10eff2cf197913afa2d50ac28144b1d2
unknown <gnsbzm@gmail.com>
2021-05-28 02:52:26 +0300

    Auto login added

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/services.dart';
-import 'package:mobx/mobx.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
