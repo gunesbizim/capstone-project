828a87132f75697ea2f44a24baa3c91161a7bf8c
hazarcandoga.bakan <hazarcandogabakan@gmail.com>
2021-05-28 15:34:47 +0300

    recommit

--- lib/views/flight_controller/view_model/flight_controller_view_model.dart
+++ lib/views/flight_controller/view_model/flight_controller_view_model.dart
@@ -1,8 +1,15 @@
+import 'package:capstone_project/core/base/model/base_view_model.dart';
 import 'package:mobx/mobx.dart';
 part 'flight_controller_view_model.g.dart';
 
 class FlightControllerViewModel = _FlightControllerViewModelBase with _$FlightControllerViewModel;
 
-abstract class _FlightControllerViewModelBase with Store {
+abstract class _FlightControllerViewModelBase with Store, BaseViewModel {
   
+  @override
+  void setContext(context) => this.context = context;
+  @override
+  void init(){
+    
+  }
 }
